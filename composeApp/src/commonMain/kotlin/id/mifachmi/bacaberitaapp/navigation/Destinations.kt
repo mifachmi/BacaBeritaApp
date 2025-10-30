2220ad8443cb0bbe6c2da3720bda7ec051e874c8
Fachmi Dimas Ardhana <fachmi2007@gmail.com>
2025-10-30 07:52:07 +0700

    feat: on progress setup navigation

---
+++ composeApp/src/commonMain/kotlin/id/mifachmi/bacaberitaapp/navigation/Destinations.kt
@@ -0,0 +1,9 @@
+package id.mifachmi.bacaberitaapp.navigation
+
+enum class Destination(
+    val route: String,
+    val label: String
+) {
+    HOME("beranda", "Beranda"),
+    BOOKMARKS("bookmarks", "Bookmarks")
+}
